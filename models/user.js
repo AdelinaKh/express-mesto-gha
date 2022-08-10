f53da34b0da39374de02860687e0320f47608855
AdelinaKh <90128968+AdelinaKh@users.noreply.github.com>
2022-08-10 21:31:13 +0300

    prj14

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const {
   ERROR_NOT_AUTHORIZED,
+  AVATAR_VALIDATOR,
 } = require('../utils/constants');
 
 const userSchema = new mongoose.Schema({
@@ -21,10 +22,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
-      validator: function(v) {
-        return /^http?:\/\/\(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/.test(v)
-      }
-    }
+      validator: (v) => AVATAR_VALIDATOR.test(v),
+    },
   },
   email: {
     type: String,
