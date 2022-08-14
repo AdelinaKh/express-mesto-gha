1b200c93be2cf38a3d8052f787a982ba4836379d
AdelinaKh <90128968+AdelinaKh@users.noreply.github.com>
2022-08-14 17:57:11 +0300

    prj14_v1

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const {
+  AVATAR_VALIDATOR,
+} = require('../utils/constants');
+
 const {
   getCards,
   deleteCardsById,
@@ -8,9 +13,30 @@ const {
 } = require('../controllers/cards');
 
 router.get('/cards', getCards);
-router.delete('/cards/:cardId', deleteCardsById);
-router.post('/cards', createCards);
-router.put('/cards/:cardId/likes', likeCard);
-router.delete('/cards/:cardId/likes', dislikeCard);
+
+router.post('/cards', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(AVATAR_VALIDATOR),
+  }),
+}), createCards);
+
+router.delete('/cards/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), deleteCardsById);
+
+router.put('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), likeCard);
+
+router.delete('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), dislikeCard);
 
 module.exports = router;
--- utils/constants.js
+++ utils/constants.js
@@ -2,7 +2,7 @@ const ERROR_CODE_BAD_REQUEST = 400;
 const ERROR_CODE_NOT_FOUND = 404;
 const ERROR_CODE_DEFAULT = 500;
 const ERROR_NOT_AUTHORIZED = 401;
-const AVATAR_VALIDATOR = /(http:\/\/|https:\/\/)(www)*[a-z0-9\S]*/;
+const AVATAR_VALIDATOR = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/;
 
 module.exports = {
   ERROR_CODE_BAD_REQUEST,
